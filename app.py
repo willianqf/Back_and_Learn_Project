aa823e7c9bc1aae1735a08e2bb048f6fee647863
Willian Quirino <willianqf1996@gmail.com>
2025-07-18 17:37:35 -0300

    Att

--- app.py
+++ app.py
@@ -132,5 +132,9 @@ scheduler.start()
 import atexit
 atexit.register(lambda: scheduler.shutdown())
 
+@app.route('/health', methods=['GET'])
+def health_check():
+    return jsonify({'status': 'ok'}), 200
+
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8080)
